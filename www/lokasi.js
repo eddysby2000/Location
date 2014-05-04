805c51ee25fcbb4c7a1273a4612a768f8b133b97
eddysby2000 <eddy_sby2000@yahoo.com>
2014-05-04 17:49:46 +0700

    Update lokasi.js

--- www/lokasi.js
+++ www/lokasi.js
@@ -2,12 +2,18 @@ var exec = require('cordova/exec');
 
 var lokasi = {
    location: function(fnSuccess, fnError){
+      /*
+       * fnSuccess:{latitude:xxx, longitude:yyy}
+       */
       exec(fnSuccess, fnError, "Lokasi", "location", []);
    },
    address: function(fnSuccess, fnError){
       exec(fnSuccess, fnError, "Lokasi", "address", []);
    },
    addressByLocation: function(fnSuccess, fnError, latitude, longitude){
+      /*
+       * fnSuccess:{address:["","",""], postalCode:"09090"}
+       */
       exec(fnSuccess, fnError, "Lokasi", "addressByLocation", [latitude, longitude]);
    }
 };
